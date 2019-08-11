54965b3c2da225b46c5c2fdbcf73a13a51ffe3bb
poopoomarvin <hchs010077@gmail.com>
2019-08-11 11:23:25 +0800

    try

--- easy/125_valid_palindrome.java
+++ easy/125_valid_palindrome.java
@@ -1,16 +1,16 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        if (s.length() == 0) return false;
+        if (s.length() == 0) return true;
         int frontIndex = 0;
         int backIndex = s.length() - 1;
         boolean b = false;
         while(frontIndex < backIndex) {
-            while(Character.isLetter(s.charAt(frontIndex)) == false){
-                if (frontIndex == s.length() - 1) return false;
+            while(isAlphanumeric(s.charAt(frontIndex)) == false){
+                if (frontIndex == s.length() - 1) return true;
                 frontIndex ++;
             }
-            while(Character.isLetter(s.charAt(backIndex)) == false) backIndex --;
-            if (s.charAt(frontIndex) == s.charAt(backIndex)) {
+            while(isAlphanumeric(s.charAt(backIndex)) == false) backIndex --;
+            if (Character.getNumericValue(s.charAt(frontIndex)) == Character.getNumericValue(s.charAt(backIndex))) {
                 frontIndex ++;
                 backIndex --;
             } else {
@@ -18,5 +18,8 @@ class Solution {
             }
         }
         return true;
+    }
+    boolean isAlphanumeric(char c) {
+        return Character.isLetter(c) || Character.isDigit();
     }
 }
--- tools/execution/MainClass.java
+++ tools/execution/MainClass.java
@@ -1,45 +1,42 @@
 import java.io.*;
-import org.json.simple.jsonarray;
 class Solution {
-    public boolean isPalindrome(String s) {
-        int frontIndex = 0;
-        int backIndex = s.length() - 1;
-        boolean b = false;
-        while(frontIndex >= backIndex) {
-            while(Character.isLetter(s.charAt(frontIndex)) == false) frontIndex ++;
-            while(Character.isLetter(s.charAt(backIndex)) == false) backIndex --;
-            if (s.charAt(frontIndex) == s.charAt(backIndex)) {
-                frontIndex ++;
-                backIndex --;
-            } else {
-                return false;
-            }
+    public int numRollsToTarget(int d, int f, int target) {
+        long ans = 0;
+        ans = roll(d, f, target, 0, 0);
+        return (int)ans;
+    }
+    long roll(int diceCount, int face, int target, long diceSum, long ans) {
+        System.out.print("diceCount:");
+        System.out.print(diceCount);
+        System.out.print(", ans:");
+        System.out.println(ans);
+        if (diceCount == 1) {
+            if (diceSum + face >= target) return ans + 1;
+            else return ans;
         }
-        return true;
+
+        for(int i = 1; i <= face; i++) {
+            ans = roll(diceCount - 1, face, target, diceSum + i, ans);
+            if (ans > 1000000007) ans = ans % 1000000007;
+        }
+        return ans;
     }
 }
 
 
 public class MainClass {
-    public static String stringToString(String input) {
-        return JsonArray.readFrom("[" + input + "]").get(0).asString();
-    }
-
-    public static String booleanToString(boolean input) {
-        return input ? "True" : "False";
-    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String line;
-        while ((line = in.readLine()) != null) {
-            String s = stringToString(line);
+        line = in.readLine();
+        int d = Integer.parseInt(line);
+        line = in.readLine();
+        int f = Integer.parseInt(line);
+        line = in.readLine();
+        int target = Integer.parseInt(line);
+        int ret = new Solution().numRollsToTarget(d, f, target);
+        System.out.println(ret);
 
-            boolean ret = new Solution().isPalindrome(s);
-
-            String out = booleanToString(ret);
-
-            System.out.print(out);
-        }
     }
 }
